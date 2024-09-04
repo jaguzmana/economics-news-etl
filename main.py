638838031d038b5db19e59df2b3c8e13b027ece6
Jorge Guzmán <jorge.guzman.arciniegas@gmail.com>
2024-09-04 11:57:36 -0500

    Modified main.py

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ from scr.pipeline.transform import transform
 from scr.pipeline.load import load
 from scr.database.conection import get_connection
 from scr.utils.logger_config import logger
+from decouple import config
 
 def main() -> None:
     try:
         logger.info("Starting Extract Data Process")
-        raw_economics_news = extract(
-            "/home/jaguzmana/projects/economics-news-scraper/data", 
-            "/home/jaguzmana/projects/economics-news-etl/scr/data/staging_extracted_data/stg_extracted.csv"
-        )
+        raw_economics_news = extract(config("DATA_LOCATION"),config("DATA_LOCATION_STG_EXTRACTED"))
         logger.info("Extract Data Process Ended")
     except Exception as e:
         logger.error(f"An error occurred during the extract process: {e}")
@@ -18,10 +16,7 @@ def main() -> None:
 
     try:
         logger.info("Starting Transform Data Process")
-        transformed_economics_news = transform(
-            raw_economics_news,
-            "/home/jaguzmana/projects/economics-news-etl/scr/data/staging_transformed_data/stg_transformed.csv"
-        )
+        transformed_economics_news = transform(raw_economics_news,config("DATA_LOCATION_STG_TRANSFORMED"))
         logger.info("Transform Data Process Ended")
     except Exception as e:
         logger.error(f"An error occurred during the transform process: {e}")
